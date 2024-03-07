bdc542c2beeb2b424930bfdcddd0a689d7b906f4
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-03-07 19:17:34 +0100

    add: aggiunto componente navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import Main from "./components/MainComponents.vue";
+import NavBar from "./components/NavComponents.vue";
 
 export default {
   components: {
     Main,
+    NavBar,
   },
 };
 </script>
 
 <template>
+  <NavBar />
   <Main />
   <router-view> </router-view>
 </template>
