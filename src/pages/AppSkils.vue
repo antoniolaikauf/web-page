3907a292e9dc43c2027f8fe1f8b056eea1400c1f
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-03-09 13:47:32 +0100

    add: aggiunto commenti

--- src/pages/AppSkils.vue
+++ src/pages/AppSkils.vue
@@ -11,63 +11,93 @@ export default {
         "indovina il numero che sta pensando CYBERIA in meno di 5 tentativi",
         "pari o dispari contro CYBERIA",
       ],
+      // array con dentro le risposte date all'utente da parte del programma
       risposta: [
+        // la prima non cambiarla
         "ok hai scelto sasso carta forbice",
         "bravo hai scelto un gioco ma siccome sono io che gestisco questa pagina e ho voglia di giocare a sasso carta e forbice giochiamo a questo",
         "è inuti che ci riprovi decido io qua",
         "quello che dico è legge",
       ],
+      // variabile per comparsa e scomparsa  tag
       nascondi_giochi: false,
+      // variabile per gioco
       scelta_utente: "",
+      // scelte del computer per gioco
       scelte: ["sasso", "carta", "forbice"],
+      // variabile che serve per il gioco cosi che il programma smetta automaticamete
       conteggio: 0,
+      // indice che serve per pescare le risposte dentro all'array di risposte
       indice_array_risposta: 1,
     };
   },
   methods: {
+    // fuzione per scelta risposte e per seleionare il gioco
     gioco_sasso_carta_forbice(index) {
+      // variabile dove verrà inserita la frase per l'utente
       const text_inizio_gioco = document.querySelector(".inizio_gioco");
       this.nascondi_giochi = true;
       if (index === 0) {
+        // fa vedere la frase base
+        // PS se si cambia il posizionamento di "sasso carta forbice con CYBERIA", nell'arry giochi bisogna cambiare l'indice di this.risposta[0]
         store.transformElement(this.risposta[0], text_inizio_gioco);
       } else {
+        // reset dell'indice
         if (this.indice_array_risposta === this.risposta.length) {
           this.indice_array_risposta = 1;
         }
+        // funzione che si trova nel file store
         store.transformElement(
           this.risposta[this.indice_array_risposta],
           text_inizio_gioco
         );
+        // incremento del indice array cosi da risposte diverse
         this.indice_array_risposta++;
       }
+      // svuotamento dello spazio dove c'è la risposta
       text_inizio_gioco.innerHTML = "";
     },
+
     all_giochi() {
+      // variabile per far vedere i giochi
       this.nascondi_giochi = false;
+
       const text_inizio_gioco = document.querySelector(".inizio_gioco");
+      // azzerato variabile
       this.conteggio = 0;
+      // svuotamento dello spazio dove c'è la risposta
       text_inizio_gioco.innerHTML = "";
 
       // let ciao = 1;
       // const text_inizio_gioco = document.querySelector(".inizio_gioco");
       // store.transformElement(this.risposta[indice], text_inizio_gioco, ciao);
     },
+    // funzione sasso carta forbice
     play() {
+      // decisione conteggio sarebbe quante volte ha deciso di giocare il computer
+      // e se è uguale a conteggio (che sarebbero quante volte ha premuto play l'utente) il comuter riporta automaticamente alla pagina play
       let decisione_computer = Math.floor(Math.random() * (10 - 3) + 3);
       if (!this.scelte.includes(this.scelta_utente)) {
+        // controllo del input dove utente iserisce il dato
         alert("devi scrivere sasso carta o forbice");
-      } else if (this.conteggio === decisione_computer) {
+      }
+      // controllo se conteggio è uguale a decisione computer
+      else if (this.conteggio === decisione_computer) {
         this.nascondi_giochi = false;
         this.conteggio = 0;
         let output_play = (document.querySelector(".output").innerHTML =
           "mi sono stancata di giocare quindi smettiamo qua");
       } else {
         this.conteggio++;
+        // numero random del computer che pescherà una valore dentro ad array risposte
         let computer_scelta = Math.floor(Math.random() * this.scelte.length);
+        // se si pareggia
         if (this.scelta_utente === this.scelte[computer_scelta]) {
           let output_play = (document.querySelector(".output").innerHTML =
             "abbiamo pareggiato");
-        } else if (
+        }
+        // se si vince
+        else if (
           (this.scelta_utente === "sasso" &&
             this.scelte[computer_scelta] == "forbice") ||
           (this.scelta_utente === "carta" &&
@@ -77,11 +107,12 @@ export default {
         ) {
           let output_play = (document.querySelector(".output").innerHTML =
             "ti è andata bene hai vinto");
-        } else {
+        }
+        // se si perde
+        else {
           let output_play = (document.querySelector(".output").innerHTML =
             "ho vinto ");
         }
-        console.log(this.scelte[computer_scelta]);
       }
     },
   },
@@ -132,4 +163,10 @@ export default {
 
 <style lang="scss">
 @import "./../style/general.scss";
+
+.inizio_gioco,
+.output {
+  background: radial-gradient(white, transparent);
+  border-radius: 15px;
+}
 </style>
