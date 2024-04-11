73d4b7caa065b3a046f7e81b59278e4497e10781
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-11 17:10:09 +0200

    add file errore

---
+++ src/pages/pagina-errore.vue
@@ -0,0 +1,11 @@
+<script>
+export default {};
+</script>
+
+<template>errore</template>
+
+<style scoped lang="scss">
+@use "./style/partials/mixins" as *;
+@use "./style/partials/variable" as *;
+@use "./style/general.scss" as *;
+</style>
