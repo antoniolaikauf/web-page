4119c08ac65e38865edfae2509459c4433a3a91a
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-11 17:40:49 +0200

    add file error

---
+++ src/components/notFound.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<template>
+  <h1>404 - Pagina Non Trovata</h1>
+  <p>Spiacente, la pagina che stai cercando non esiste o è stata rimossa.</p>
+  <a class="nav-link active fs-4 text-font" aria-current="page" href="#"><router-link :to="{ name: 'home' }"> Home </router-link></a>
+</template>
+
+<style scoped lang="scss"></style>
