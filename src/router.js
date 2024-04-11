ea7f7c03e7ffdc0687ae442a20d8acc4efd36bd8
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-11 18:32:17 +0200

    e

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 // importazione file per rotte 
 import AppGioco from './pages/AppGioco.vue';
 import AppHome from "./pages/AppHome.vue";
-import notFound from "./components/notFound.vue";
 
  
 // creazione delle rotte e il loro 'percorso'
@@ -18,12 +17,7 @@ const router = createRouter({
       path: "/gioco",
       name: "gioco",
       component:AppGioco
-    },
-    {
-      path: '/*',
-      name: 'not-found',
-      component: notFound,
-    },
+    }, 
   ],
 });
 export { router };
