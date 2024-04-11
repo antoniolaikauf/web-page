3f4f39f3d43945468ee9516fcea83c19cf60f8d7
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-11 19:10:08 +0200

    add file netfly

---
+++ src/components/errorpage.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "error",
+};
+</script>
+
+<template>ciaooooo</template>
+
+<style scoped lang="scss"></style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 // importazione file per rotte 
 import AppGioco from './pages/AppGioco.vue';
 import AppHome from "./pages/AppHome.vue";
+import error  from "./pages/errorpage.vue"
 
  
 // creazione delle rotte e il loro 'percorso'
@@ -17,7 +18,12 @@ const router = createRouter({
       path: "/gioco",
       name: "gioco",
       component:AppGioco
-    }, 
+    },
+    {
+      path: '/:pathMatch(.)',
+      name: 'notfound',
+      component: error
+    },
   ],
 });
 export { router };
