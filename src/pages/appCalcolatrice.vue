9975ef5f422464b98813b978e49c19597b4319ca
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-30 15:56:11 +0200

    add: aggiunto metodo per ottenere numeri

--- src/pages/appCalcolatrice.vue
+++ src/pages/appCalcolatrice.vue
@@ -4,17 +4,23 @@ export default {
 
   data() {
     return {
-      pulsanti: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-      operazione: ["+", "-", "x", "/"],
+      pulsanti: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-", "x", "/", "="],
+      risultato: "fffffffff",
     };
+  },
+  methods: {
+    number_selected(Nindex) {
+      console.log(Nindex);
+    },
   },
 };
 </script>
 <template>
   <section>
     <div class="calculator">
-      <div v-for="(pulsante, i) in pulsanti">{{ pulsante }}</div>
+      <div class="number" v-for="(pulsante, i) in pulsanti" @click="number_selected(i)">{{ pulsante }}</div>
     </div>
+    <div>{{ risultato }}</div>
   </section>
 </template>
 <style lang="scss" scoped>
@@ -24,8 +30,15 @@ export default {
   border: 1px solid black;
   display: flex;
   flex-wrap: wrap;
-  div {
-    width: 20%;
+  .number {
+    background-color: black;
+    margin: 10px;
+    width: calc((100% / 5) - 20px);
+  }
+  .operator {
+    margin: 10px;
+    width: calc((100% / 4) - 20px);
+    background-color: black;
   }
 }
 </style>
