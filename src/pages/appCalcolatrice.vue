fe1b6805a1dbe72a18accae73deb379523f4b071
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-30 18:18:42 +0200

    add: aggiunto output della calcolatrice

--- src/pages/appCalcolatrice.vue
+++ src/pages/appCalcolatrice.vue
@@ -5,23 +5,28 @@ export default {
   data() {
     return {
       pulsanti: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-", "*", "/", "="],
-      risultato: " ",
       console: "",
       number: "",
+      result: "",
     };
   },
   methods: {
-    number_selected(Nindex) {
+    calculation(Nindex) {
       this.console += this.pulsanti[Nindex]; // concatena tutta la stringa
-      //   if (this.console === "") alert("inserisci un numero");
-      if ("=" === this.pulsanti[Nindex] && this.pulsanti.length >= 2) this.calculation(this.number);
       var regex = /[\+\-\*\/]|(\d+)/g; // prima parte corrisponde a tutti gli operatori la seconda parte corrisponde ai numeri
       this.number = this.console.match(regex); //  ritorna un array che con delle corrispondenze con l'espressione inserita dentro il metodo match
+      console.log(this.number);
+      console.log(this.number.length);
+      if ("=" === this.pulsanti[Nindex] && this.number.length <= 2) {
+        // controllo se utente ha inserito almeno un operatore e due numeri
+        alert("devi inserire un operatore o piu numeri ");
+        this.deleteResult();
+      } else if ("=" === this.pulsanti[Nindex] && this.number.length >= 2) this.result = eval(this.number.join(" ")); // trasforma una stringa e la esegue come se fosse del codice
     },
-    calculation(ArrayNumebers) {
-      for (let i = 0; i < ArrayNumebers.length; i++) {
-        console.log(ArrayNumebers[i]);
-      }
+    deleteResult() {
+      this.result = "";
+      this.number = "";
+      this.console = "";
     },
   },
 };
@@ -29,12 +34,18 @@ export default {
 <template>
   <section>
     <div class="calculator">
-      <div class="number" v-for="(pulsante, i) in pulsanti" @click="number_selected(i)">{{ pulsante }}</div>
+      <div class="number" v-for="(pulsante, i) in pulsanti" @click="calculation(i)">{{ pulsante }}</div>
     </div>
-    <div>{{ risultato }}</div>
+    <div>
+      <button class="btn-page my-3" @click="deleteResult">Cancella risultato</button>
+    </div>
+    <h5>{{ result }}</h5>
   </section>
 </template>
 <style lang="scss" scoped>
+@use "./../style/partials/mixins" as *;
+@use "./../style/general.scss" as *;
+@use "./../style/partials/variable" as *;
 .calculator {
   width: 50%;
   margin: auto;
