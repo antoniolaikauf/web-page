8ff237ba03e667780cbc7bb6c6d34f605a8c1f70
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-10 18:00:59 +0200

    aggiunto immagine file notfound

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -5,13 +5,29 @@ export default {
 </script>
 
 <template>
-  <h1>Torna indietro se no CYBERIA si arrabbia !!</h1>
+  <section class="d-flex">
+    <div>
+      <h1>Torna indietro se no CYBERIA si arrabbia !!</h1>
+    </div>
+    <div>
+      <img src="/public/img/face1.jpg" alt="" class="face" />
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
 @use "./../style/general.scss" as *;
 @use "./../style/partials/variable" as *;
- h1{
-  font-family: $font_cyberia;
- }
+
+section {
+  h1 {
+    font-family: $font_cyberia;
+  }
+  div {
+    width: 50%;
+  }
+  .face {
+    width: 50%;
+  }
+}
 </style>
