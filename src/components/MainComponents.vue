510aea1c86311653b5ba8b3c73a90c147eb7a2b0
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-11 18:01:59 +0200

    cambiato contenitore

--- src/components/MainComponents.vue
+++ src/components/MainComponents.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-  <div class="container-fluid">
+  <div class="container">
     <div class="row">
       <section class="text-center">
         <router-view> </router-view>
