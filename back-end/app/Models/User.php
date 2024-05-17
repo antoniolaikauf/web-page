b0e80a59298680d1b68f5984f048c91aab8f10df
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-17 16:51:36 +0200

    creato collegamento tabelle

--- back-end/app/Models/User.php
+++ back-end/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    function messages()
+    {
+        $this->belongsTo(message::class);
+    }
 }
