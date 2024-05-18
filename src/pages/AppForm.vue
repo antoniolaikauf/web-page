606274f221fa82a6293c242347ebadeb4cb255a8
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-18 16:34:32 +0200

    aggiunto form dati

--- src/pages/AppForm.vue
+++ src/pages/AppForm.vue
@@ -6,10 +6,29 @@ export default {
   },
 };
 </script>
-<template>cigfoiff</template>
+<template>
+  <section>
+    <div>
+      <form action="Get">
+        <div>
+          <label for="name">Inserire Nome</label> <br />
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label for="email">Inserire email</label> <br />
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label for="password"> Inserire password</label> <br />
+          <input type="password" id="password" />
+        </div>
+        <input type="submit" value="Accedi" />
+      </form>
+    </div>
+  </section>
+</template>
 <style lang="scss" scoped>
 @use "./../style/general.scss" as *;
 @use "./../style/partials/mixins" as *;
 @use "./../style/partials/variable" as *;
-
 </style>
