930e102da0461794ef5980a621bbfc1720bf9e5d
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-22 16:06:47 +0200

    style file group

--- src/pages/AppGroup.vue
+++ src/pages/AppGroup.vue
@@ -14,7 +14,7 @@ export default {
 <template>
   <section>
     <h3>Gruppo generale</h3>
-    <div class="messaggi"></div>
+    <div class="messaggi">ggggggggggggg</div>
   </section>
 </template>
 <style scoped lang="scss">
@@ -22,12 +22,35 @@ export default {
 @use "./../style/general.scss" as *;
 @use "./../style/partials/variable" as *;
 .messaggi {
-  width: 70%;
+  margin: auto;
+  width: 80%;
   height: 70vh;
   overflow-y: scroll;
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 20px;
+  backdrop-filter: blur(10px);
 }
 
+/* width */
 ::-webkit-scrollbar {
-  display: none;
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey;
+  border-radius: 8px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: purple;
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #b30000;
 }
 </style>
